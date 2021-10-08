27939e51686967cea2aa2a2435b03e87bd2a9953
Kemal Korucu <kemal@korucutech.com>
2021-10-08 10:53:50 -0400

    Create aws-install-docker.sh

---
+++ Scripts/aws-install-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install docker on Amazon Linux 2
+
+sudo yum update -y
+  
+sudo amazon-linux-extras install docker
+
+sudo service docker start
+
+sudo usermod -a -G docker ec2-user
+
+docker info
+
+# Install docker-compose
+
+sudo chkconfig docker on
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose version
